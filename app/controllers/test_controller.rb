91c2b0cefa1d5a024d64e8480e09f6bfef327487
Andrew Max <anm898989@gmail.com>
2016-10-21 21:35:52 -0400

    adds pry debugger

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,8 @@
 class TestController < ApplicationController
 
   def jeff
+    # uncommenting binding.pry will freeze code execution and give you an interactive command line in your rails server
+    # binding.pry
     @text = "hello"
   end
 end
