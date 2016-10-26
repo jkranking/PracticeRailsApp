b29a2e13404619bfa6adb4dbf38a0056a3f84bef
Jeffrey Kranking <jkranking@gmail.com>
2016-10-25 20:44:12 -0700

    adds accounts_users and users_show/index

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,4 +8,8 @@ class AccountsController < ApplicationController
     @account = Account.find(params[:id])
   end
 
+  def users_index
+    @account = Account.find(params[:id]).users 
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+root to: 'accounts#index'
+
 get 'test', to: 'test#jeff'
 
 get '/accounts', to: 'accounts#index'
 
 get '/accounts/:id', to: 'accounts#show', as: 'account'
+
+get '/accounts/:id/users', to: 'accounts#users_index', as: 'accounts_users'
+
+get '/users', to: 'users#index'
+
+get '/users/:id', to: 'users#show', as: 'user'
 
 end
