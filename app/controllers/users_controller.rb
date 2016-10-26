476f6cfded9f4fea50db00c5f64cb8a24d87a7b7
Andrew Max <anm898989@gmail.com>
2016-10-26 14:32:00 -0400

    touch up on users index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
 
   def index
-    if params[:account][id:] 
+    if params[:account_id]
       @users = Account.find(params[id:]).users
-    else @users = User.all
-    end 
-  end 
+    else
+      @users = User.all
+    end
+  end
 
   def show
     @user = User.find(params[:id])
   end
 
-end 
+end
