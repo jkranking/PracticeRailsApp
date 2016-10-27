3ccfe18bdf90382518aba4ff7a90aac0f1f3cf44
Jeffrey Kranking <jkranking@gmail.com>
2016-10-27 14:35:28 -0700

    maps account_users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
 root to: 'accounts#index'
 
-# get '/accounts/:id/users', to: 'accounts#users_index', as: 'accounts_users'
-
   resources :users, only: [:index, :show]
 
   resources :accounts, only: [:index, :show] do 
