3a9a5216f035c461f484b7801dc4f013fe2c7ecd
Jeffrey Kranking <jkranking@gmail.com>
2016-10-28 11:38:46 -0700

    fixes comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'home#home'
 
-  resources :users, only: [:index, :show, :create, :new, :update]
+  resources :users, except: [:edit, :update]
 
   resources :accounts, only: [:index, :show] do 
     resources :users, only: [:index, :show]
